ca8248ecb7b59edf09f7bd0a6a5e91b36b703810
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 07:42:50 -0700

    message

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=Kishore
+PERSON2=Pujitha
+
+
+echo "$PERSON1: Hi $PERSON2 , how are you"
+echo "$PERSON2: Hi $PERSON1 , Iam fine ! How are you ? "
+echo "$PERSON1: Iam fine ! How was your weekend ? "
+echo "$PERSON2: Its good ! I went to HIT3 movie"
+echo "$PERSON1: Ohh great ! "
