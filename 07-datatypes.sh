f32fe8c6d9ae5aeb4319096bfb14f3f3237935b1
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 08:06:44 -0700

    message

---
+++ 07-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("BB@"2 "INDIAN2" "ROBO2" "Thandel")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
+echo "All movies: ${MOVIES[@]}"
