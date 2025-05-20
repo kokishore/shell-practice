b5ea8fa8388d65e1fc1e28746a65d00aeb60755d
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 07:44:36 -0700

    message

---
+++ 04-variablesasargs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hi $PERSON2 , how are you"
+echo "$PERSON2: Hi $PERSON1 , Iam fine ! How are you ? "
+echo "$PERSON1: Iam fine ! How was your weekend ? "
+echo "$PERSON2: Its good ! I went to HIT3 movie"
+echo "$PERSON1: Ohh great ! "
