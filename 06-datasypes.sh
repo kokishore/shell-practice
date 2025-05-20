bfd5bbdb40af74944412f30b9fc1c44c329ee0d7
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 08:01:47 -0700

    message

--- 06-datasypes.sh
+++ 06-datasypes.sh
@@ -6,3 +6,11 @@ Number2=300
 SUM=$(($Number1+$Number2))
 
 echo " The sum of $Number1 and $Number2 is : $SUM"
+
+
+Number3=$1
+Number4=$2
+
+SUM=$(($Number3+$Number4))
+
+echo " The sum of $Number3 and $Number4 is : $SUM"
