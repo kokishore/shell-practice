85c7a7a5570f288c70c5f1b872398e5d9a16c144
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 07:36:57 -0700

    conversation

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Kishore: Hi Pujitha , how are you"
+echo "Pujitha: Hi Kishore , Iam fine ! How are you ? "
+echo "Kishore: Iam fine ! How was your weekend ? "
+echo "Pujitha: Its good ! I went to HIT3 movie"
+echo "Kishore: Ohh great !
