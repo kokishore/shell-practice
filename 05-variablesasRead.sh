6a586b1e58e78d7590338901998b95c581cbcf84
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-20 07:50:58 -0700

    message

--- 05-variablesasRead.sh
+++ 05-variablesasRead.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+TIMESTAMP=$(date)
+
 echo " Enter your PIN Number: "
 read PIN
 echo "your PIN Number is $PIN "
+
+echo " Enter your PIN Number: "
+read -s PIN
+echo "your PIN Number is $PIN "
+echo "script executed at this $TIMESTAMP"
