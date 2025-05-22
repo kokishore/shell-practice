94cf8283c3c9c725cf45fa9478eb2a4bf4880095
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-21 18:48:17 -0700

    message

---
+++ 08-specialvariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "To pass all the varialbles we can use : $@ "
+echo "To know the name of the script : $0 "
+echo "To know the current working directory : $PWD "
+echo "To know home directory of the user : $HOME "
+echo "To Know the current user : $USER "
+echo "To know number variables passed to the script : $# "
+echo " To know the PID of the script : $$ "
+sleep 20 &
+ech0 "To know the PID of the which is running in the background : $! "
