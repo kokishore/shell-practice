85f47ffae2ca58a65142fc878c400ba9bd4db838
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-27 05:40:51 -0700

    conditions

--- 11-install-commands.sh
+++ 11-install-commands.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
      echo "error:you are not running with root access Please run with root access "
+     exit 1
 else    
     echo "You already running with root access "
 fi 
