0c6eff0ab5c5e4f55b8e388123b0460827ca0476
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-27 05:54:02 -0700

    conditions

--- 11-install-commands.sh
+++ 11-install-commands.sh
@@ -8,12 +8,20 @@ then
 else    
     echo "You already running with root access "
 fi 
-dnf install mysqlzzzzz -y
-if [ $? -eq 0 ]
-then
+dnf list installed mysql 
+if [ $? -ne 0 ]
+then 
+    echo "My Sql is not installed....We are going to install it now"
+dnf install mysql -y
+    if [ $? -eq 0 ]
+   then
     echo "Installing Mysql is ....Success"
-else
+   else
     echo "Installing Mysql is .... Failure"
     exit 1
-fi
+    fi
+
+else
+ echo " My sql is already installed .... nothing to do "
+ fi
 
