a8aaa2625e5731e23b139e9c3050de6311b94adf
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-05-27 04:41:10 -0700

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Number=$1
+
+if [ $Number is -lt 10 ]
+then 
+    echo " The $Number is lessthan 10 "
+else
+    echo " The $Number is greaterthan 10 "
+fi
+
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal
+# -ne --> not equal
